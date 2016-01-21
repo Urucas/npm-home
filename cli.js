f064e0c89a36747c3096a443cfb3a14a5c5552d5
vrunoa <vruno.alassia@gmail.com>
2016-01-20 16:55:45 -0800

    add -l,-list flag. Let the user choose & open between the project dependencies

--- cli.js
+++ cli.js
@@ -3,13 +3,42 @@
 const meow = require('meow');
 const readPkgUp = require('read-pkg-up');
 const opn = require('opn');
+const inquirer = require('inquirer');
 
-meow(`
+var cli = meow(`
 	Usage
 	  $ npm-home
 	  $ nh
-`);
+	Options
+	  -l, -list Show package dependencies
+`, {
+	alias: {
+		l: 'list'
+	}
+});
+
+function open(pkg) {
+	opn(`https://www.npmjs.com/package/${pkg}`, {wait: false});
+}
+
+function prompt(pkg, dependencies, devDependencies) {
+	var pkgs = [pkg]
+	for(var key in dependencies) pkgs.push(key)
+	for(var key in devDependencies) pkgs.push(key)
+	inquirer.prompt({
+		type: 'checkbox',
+		name: 'pkg',
+		message: 'Choice a package',
+		choices: pkgs,
+		default: [pkg]
+	}, function(answer){
+		open(answer.pkg)
+	})
+}
 
 readPkgUp().then(result => {
-	opn(`https://www.npmjs.com/package/${result.pkg.name}`, {wait: false});
+	if(cli.flags.l || cli.flags.list) {
+		prompt(result.pkg.name, result.pkg.dependencies || {}, result.pkg.devDependencies || {})
+	}else
+		open(result.pkg.name)
 });
